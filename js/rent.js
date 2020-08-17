565dde8680c100e971047febaf23de07201d4f2f
v5ln <68195919+v5ln@users.noreply.github.com>
2020-08-17 17:52:11 +0300

    add js
    add light/dark mode in home page and css file for light mode.
    add filter for home page search action.
    add a filter for rent page filter.

--- js/rent.js
+++ js/rent.js
@@ -6,3 +6,33 @@ function myfun1(){
 	 document.getElementById("for").style.display = "block";
 	document.getElementById("fo").style.display = "none";
 }
+var family = document.getElementsByClassName("family");
+var std = document.getElementsByClassName("std");
+function filter_student() {
+    for (i=0;i<family.length;i++){
+        document.getElementsByClassName("family")[i].style.display="none";
+    }
+    for (i=0;i<std.length;i++){
+        document.getElementsByClassName("std")[i].style.display="block";
+    }
+    $(document).ready(function(){
+        $("#carouselExampleIndicators").carousel("pause");
+    });
+    document.getElementById('prev').style.display='none';
+    document.getElementById('next').style.display='none';
+    document.getElementById('ind').style.display='none';
+}
+function filter_family() {
+    for (i=0;i<family.length;i++){
+        document.getElementsByClassName("family")[i].style.display="block";
+    }
+    for (i=0;i<std.length;i++){
+        document.getElementsByClassName("std")[i].style.display="none";
+    }
+    $(document).ready(function(){
+        $("#carouselExampleIndicators").carousel("cycle");
+    });
+    document.getElementById('prev').style.display='block';
+    document.getElementById('next').style.display='block';
+    document.getElementById('ind').style.display='block';
+}
